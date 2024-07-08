7c45dc28823ea0ad4d097cff577d3947da977a0d
oPestana <jonathan.pestana@cloudwalk.io>
2024-07-08 02:40:46 -0300

    feat: list of images with title and date

---
+++ android/app/src/main/kotlin/dev/pestana/picture_of_the_day/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.pestana.picture_of_the_day
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/picture_list_page.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class PictureListPage extends StatefulWidget {
+  const PictureListPage({super.key});
+
+  @override
+  State<PictureListPage> createState() => _PictureListPageState();
+}
+
+class _PictureListPageState extends State<PictureListPage> {
+  Future<List> getData() async {
+    http.Response response;
+    final url = Uri.parse(
+        'https://api.nasa.gov/planetary/apod?count=5&api_key=6MZbidRebIh1HtWdJOAbwDAIbcQrwGnXh3f4Ahoq');
+    response = await http.get(url);
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData().then((map) {
+      print('foi');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Astronomy Picture of the Day'),
+        backgroundColor: Colors.grey,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          const TextField(
+            decoration: InputDecoration(
+              labelText: 'Pesquise por nome',
+              labelStyle: TextStyle(color: Colors.white),
+            ),
+          ),
+          Expanded(
+              child: FutureBuilder(
+                future: getData(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<dynamic> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Container(
+                        alignment: Alignment.center,
+                        child: const CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(Colors.white),
+                        ),
+                      );
+                    default:
+                      final data = snapshot.data!;
+                      return ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final item = data[index];
+                          return Card(
+                            color: Colors.grey,
+                            child: Column(
+                              children: [
+                                Image.network(item['url'], fit: BoxFit.cover),
+                                Text(item['title'],
+                                  style: const TextStyle(color: Colors.white),),
+                                Text(item['date'],
+                                  style: const TextStyle(color: Colors.white),),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                  }
+                },
+              ))
+        ],
+      ),
+    );
+  }
+}
