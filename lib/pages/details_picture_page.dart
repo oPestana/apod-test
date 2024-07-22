4afb151fcf6b3dba161ee5d34583a657abd839f0
oPestana <jonathan.pestana@cloudwalk.io>
2024-07-22 16:27:35 -0300

    refactor: refatorando controle de estado e lógica de pesquisa por nome

--- lib/pages/details_picture_page.dart
+++ lib/pages/details_picture_page.dart
@@ -1,37 +1,39 @@
 import 'package:flutter/material.dart';
+import '../models/picture_model.dart';
 
 class DetailsPicture extends StatelessWidget {
   const DetailsPicture({super.key, required this.picture});
 
-  final Map picture;
+  final PictureModel picture;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(picture['title']),
+        title: Text(picture.title),
         backgroundColor: Colors.grey,
       ),
       backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(picture['url']),
+            Image.network(picture.url),
             Text(
-              picture['title'],
+              picture.title,
               style: const TextStyle(
                 color: Colors.white,
                 fontSize: 20,
               ),
             ),
             Text(
-              picture['date'],
+              picture.date,
               style: const TextStyle(color: Colors.white),
             ),
-            Text(
-              picture['explanation'],
-              style: const TextStyle(color: Colors.white),
-            ),
+            if (picture.explanation != null)
+              Text(
+                picture.explanation!,
+                style: const TextStyle(color: Colors.white),
+              ),
           ],
         ),
       ),
